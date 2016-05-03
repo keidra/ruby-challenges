8a3926699855a8bbd7f4ee751e347bab62ca4d58
keidra <keidrabaldwin@gmail.com>
2016-05-03 08:33:15 -0700

    calculator complete

---
+++ bank.rb
@@ -0,0 +1,18 @@
+balance = 4000
+
+puts "Your current balance is #{balance}."
+
+puts "What would you like to do? (deposit, withdraw, check_balance)"
+answer = gets.chomp
+
+puts "How much would you like to deposit?"
+answer = gets.chomp
+
+puts "Your current balance is #{balance}"
+
+
+puts "Are you done?"
+answer = gets.chomp
+
+puts "Thank you!"
+
--- calculator.rb
+++ calculator.rb
@@ -1,49 +1,70 @@
 class Calculator
 
-
-puts "What calculation would you like to do? (add, sub, mult, div)"
-answer = gets.chomp
+attr_accessor :operator, :number1, :number2
 
 
+def initialize(operator, number1, number2)
+  @operator = operator
+  @number1 = number1
+  @number2 = number2
+  @sum = 0
+end
 
-puts "What is number 1?"
-number1 = gets
-
-puts "What is number 2?"
-number2 = gets
-
+def calculate (operator, number1, number2)
 
 def add(number1, number2)
-  sum = number1 + number2
-self
+  @sum = number1 + number2
 end
 
 def subtract(number1, number2)
-  sum = number1 - number2
-self
+  @sum = number1 - number2
+
 end
 
 def multiply(number1, number2)
-  sum = number1 * number2
-self
+  @sum = number1 * number2
 end
 
 def divide(number1, number2)
- sum = number1 % number2
-self
+ @sum = number1 % number2
+end
+
+if @operator == "add"
+  add(number1, number2)
+  return @sum
+end
+
+if @operator == "sub"
+  subtract(number1, number2)
+  return @sum
+end
+
+if @operator == "mul"
+  multiply(number1, number2)
+  return @sum
+end
+
+if @operator == "div"
+  divide(number1, number2)
+  return @sum
+end
 end
 end
 
-puts Calculator
-# if answer == "add" 
-  
-# elsif answer == "sub"
-#  sum = 
-# elsif answer == "mult"
-#   sum = number1 * number2
-# end
-# end
-      
 
-# puts "your result is #{sum}" 
+puts "What calculation would you like to do? (add, sub, mult, div)"
+operator = gets.chomp
+
+puts "What is number 1?"
+number1 = gets.chomp
+
+puts "What is number 2?"
+number2 = gets.chomp
+
+
+final = Calculator.new(operator,number1,number2)
+
+
+puts "Your result is:"
+puts final.calculate(operator, number1, number2)
 
