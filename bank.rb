b1e61a7a9327ccf01b6e80dbc4d0f382e7051a6d
keidra <keidrabaldwin@gmail.com>
2016-05-03 08:58:39 -0700

    bank almost works

--- bank.rb
+++ bank.rb
@@ -1,18 +1,43 @@
-balance = 4000
 
-puts "Your current balance is #{balance}."
+  @balance = 4000
+
+
+def deposit(bal, money)
+  balance = bal + money
+  puts "Your current balance is #{balance}."
+end
+
+def withdraw(bal, money)
+balance = balance - money
+puts "Your current balance is #{balance}"
+end
+
+def check_balance(bal)
+  puts "Your current balance is #{bal}"
+end
+
+
+
+puts "Your current balance is #{@balance}"
 
 puts "What would you like to do? (deposit, withdraw, check_balance)"
-answer = gets.chomp
+result = gets.chomp
 
 puts "How much would you like to deposit?"
-answer = gets.chomp
+money = gets.chomp.to_f
 
-puts "Your current balance is #{balance}"
+
+if result == 'deposit'
+  deposit(bal, money)
+elsif result == 'withdraw'
+  withraw(bal, money)
+elsif result == 'check_balance'
+  check_balance(bal)
+end
+
 
 
 puts "Are you done?"
 answer = gets.chomp
-
 puts "Thank you!"
 
